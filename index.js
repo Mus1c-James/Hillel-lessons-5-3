3b56f8a648521092d69bf5068aa3f3629f6b4203
Mus1c-James <ditmer2@gmail.com>
2025-01-16 19:06:42 +0200

    git remote add origin https://github.com/Mus1c-James/Hillel-lessons-12-2.git

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 "use strict";
  
-// const inputNumber = prompt('Please enter an integer N');
-let inputNumber = prompt('Please enter an integer N');
-
-inputNumber = inputNumber.trim();
-// console.log('userInput', inputNumber)  
+const textBlock = document.getElementById('text');
+const btnElement = document.getElementById('btn');
  
+btnElement.addEventListener('click', () => {
+    textBlock.classList.toggle('color-red'); 
+}); 
 
-if(inputNumber ==='' || isNaN(inputNumber) || inputNumber === '.' || inputNumber === ' ' || !Number.isInteger(+inputNumber)){
-    console.log('Error incorected number')
-}else{
-    let numN  = parseInt(inputNumber);
-    console.log('parsetIn', numN);
-    if(isNaN(numN)){
-        console.log('err', numN)
-    }else{
-       console.log('else', numN) 
-       for (let count = 1; count <= 100; count++ ){
-            if(count * count <= numN){
-                console.log(count)
-            }else{
-                break;
-            }
-       
-        }
-    }
-    
-}
+ 
